47a4709e24f3365bca4a43bc4c398ab9d371ec4e
Kn´N <kevinno@student.chalmers.se>
2023-04-19 19:20:24 +0200

    Register user duplicate check

--- src/pages/api/register-user.ts
+++ src/pages/api/register-user.ts
@@ -4,19 +4,28 @@ import { connect } from "../../utils/connection";
 import { UserModel } from "../../models/User";
 import bcrypt from "bcrypt";
 
-
-
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-   if(req.method === "POST") {
-    const {email, password } = req.body;
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const conn = await connect()
-         // Creates a new user and stores it in the database
-        .then( () => { const user = new UserModel({email: email, password: hashedPassword}); console.log(req.body); user.save();})
-        .then( (user) => {res.status(201).json(user);})
-        .catch((error) => {console.error(error); res.status(500).send("Error storing in database");})
+   if (req.method === "POST") {
+      const { email, password } = req.body;
+      const hashedPassword = await bcrypt.hash(password, 10);
+      try {
+         const conn = await connect();
+         const duplicate = await UserModel.findOne({ email: email });
+         if (duplicate) {
+            res.status(400).json({ Error: "User already exists" })
+         }
+         const user = new UserModel({ email: email, password: hashedPassword });
+         console.log(req.body);
+         await user.save();
+         res.status(200).json({ Message: "User created" });
+      } catch (error) {
+         console.error(error);
+         res.status(500).json({ Error: "Error storing in DB" });
+      }
    }
    else {
-    res.status(405).send("Only post allowed!")
+      res.status(405).json({ Message: "Only post allowed" });
    }
+
+
 }
--- src/pages/api/validate-user.ts
+++ src/pages/api/validate-user.ts
@@ -7,25 +7,25 @@ import bcrypt from "bcrypt";
 
 // Validates a user with bcrypt.compare.
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-   if(req.method === "POST") {
-    const {email, password } = req.body;
-    const conn = await connect()
-        .then(async () => {
-            const user = await UserModel.findOne({email});
-            if(!user) {
-                res.status(404).json({Message: "No user found..."});
-            }
-            else {
-                const validPassword = await bcrypt.compare(password, user.password);
-                if (validPassword) {
-                    res.status(200).json({Message: "Valid credentials! "})
+    if (req.method === "POST") {
+        const { email, password } = req.body;
+        const conn = await connect()
+            .then(async () => {
+                const user = await UserModel.findOne({ email });
+                if (!user) {
+                    res.status(404).json({ Message: "No user found..." });
                 }
                 else {
-                    res.status(401).json({Message: "Wrong password or email..."})
-                }
+                    const validPassword = await bcrypt.compare(password, user.password);
+                    if (validPassword) {
+                        res.status(200).json({ Message: "Valid credentials! " })
+                    }
+                    else {
+                        res.status(401).json({ Message: "Wrong password or email..." })
+                    }
 
-            }
-        })
-        .catch((error) => {res.status(500).json({Error: error})})
-}
+                }
+            })
+            .catch((error) => { res.status(500).json({ Error: error }) })
+    }
 }
