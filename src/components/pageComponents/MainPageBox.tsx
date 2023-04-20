8f1d6ffdb80caad5e32a8576a2056a0bb7845dd9
Kn´N <kevinno@student.chalmers.se>
2023-04-20 22:19:31 +0200

    Indexpage style

--- src/components/pageComponents/MainPageBox.tsx
+++ src/components/pageComponents/MainPageBox.tsx
@@ -8,12 +8,12 @@ interface BoxProps {
 
 export const MainPageBox = (props:BoxProps) => {
     return (
-        <div className="relative w-auto h-[500px] flex items-center justify-start flex-col bg-slate-500 flex-wrap overflow-hidden">
+        <div className="relative w-auto h-[500px] flex items-center justify-start flex-col  flex-wrap overflow-hidden">
             <img src={props.imgUrl} className=" w-full h-full object-cover"/>
 
             <div className="absolute inset-0 z-10 flex flex-col gap-10 items-center justify-baseline pt-8 ">
-            <h1 className="z-10 text-red-300 font-bold text-3xl">{props.text1}</h1>
-            <h1 className="z-10 text-red-300 font-extrabold text-3xl">{props.text2}</h1>
+            <h1 className="z-10 text-white font-bold text-3xl">{props.text1}</h1>
+            <h1 className="z-10  text-white font-extrabold text-3xl">{props.text2}</h1>
             </div>
             <button className="absolute bottom-0 "> Hello </button>
             
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,45 +1,39 @@
 import { FrameWorks } from '@/components/pageComponents/Frameworks';
 import { MainPageBox } from '@/components/pageComponents/MainPageBox';
-import { Showcase} from '@/components/pageComponents/Showcase';
+import { Showcase } from '@/components/pageComponents/Showcase';
 
 export default function Index(props: any) {
   return (
     <>
 
-      <main className="flex items-center justify-baseline pt-20 flex-col gap-40  min-h-screen w-screen overflow-hidden">
-        <div className="flex  h-screen w-screen items-center  flex-col">
 
-          <div className="text-8xl font-extrabold text-logo ">
+      <div className="flex h-screen w-screen items-center flex-col">
+        <div className="text-8xl font-extrabold text-logo ">
           <h1 >This is an e-shop.</h1>
           <h1>Buy Something..</h1>
-          </div>
-
-          <div className='w-screen h-screen flex flex-row  justify-evenly p-10 gap-10 '>
-          <MainPageBox text1="Spring sale" text2="Up to 60% off!" imgUrl="/images/fashion/pexels-ksenia-chernaya-3965557.jpg" />
-          <MainPageBox text1="" text2="" imgUrl="/images/fashion/pexels-leticia-ribeiro-2112651.jpg"/>
-          <MainPageBox text1="" text2="" imgUrl="/images/fashion/pexels-polina-tankilevitch-5585858.jpg"/>
-          </div>
-          
         </div>
-
-        <div className=" flex items-start jusify-center flex-col gap-40 ">
-          <h1 className="font-bold text-6xl p-10"> </h1>
-          <FrameWorks />
+        <div className='w-screen h-fit flex flex-row  justify-evenly m-10 '>
+          <MainPageBox text1="Spring sale" text2="Up to 60% off!" imgUrl="/images/index/home1.jpg" />
+          <MainPageBox text1="" text2="" imgUrl="/images/index/home2.jpg" />
+          <MainPageBox text1="" text2="" imgUrl="/images/index/home3.jpg" />
         </div>
-        
-      </main>
+      </div>
+      <FrameWorks />
     </>
   )
 }
+
+/*
 export async function getServerSideProps() {
   try {
     const res = await fetch("http://localhost:3000/api/getAll-product");
-      const products = await res.json();
-      return { props: { products } };
+    const products = await res.json();
+    return { props: { products } };
   }
   catch (error) {
-      console.error(error);
-      return { props: {products: []}}
+    console.error(error);
+    return { props: { products: [] } }
   }
 
 }
+*/
