cad6c0fe6e022a09f2877be20e20d68ee5ad161d
Kevin <kevin.norlin@live.com>
2023-05-11 20:28:55 +0200

    ö

--- src/components/product/Product.tsx
+++ src/components/product/Product.tsx
@@ -3,6 +3,7 @@ import { ProductSpec, ProductType } from "@/utils/types/productTypes";
 import { firstToUpper } from "@/utils/functions/stringfunctions";
 import { useState } from "react";
 import { BsFillBoxSeamFill } from "react-icons/bs";
+import { AiOutlineFullscreenExit } from "react-icons/ai";
 export const Product: React.FC<ProductType> = (props: ProductType) => {
     const [hovering, setHovering] = useState(false);
     const [clicked, setClicked] = useState(false);
@@ -27,8 +28,11 @@ export const Product: React.FC<ProductType> = (props: ProductType) => {
         <div onClick={() => setClicked(!clicked)} onMouseEnter={handleEnterHovering} onMouseLeave={handleLeaveHovering} key={props.name}
             className={`rounded-xl flex flex-col bg-main jusify-baseline 
             items-center basis10 gap-5 p-10 
-            ${clicked ? "h-full w-screen absolute p-40 top-20 left-0 right-0" : "w-96 h-96"}
+            ${clicked ? "h-full w-fill absolute p-40 z-100 top-20 left-0 right-0" : "w-96 h-96"}
             shadow-2xl transition-all ${hovering && "scale-110 cursor-pointer"}`}>
+                <div className="absolute bg-red-500 right-20 top-30">
+                {clicked && <AiOutlineFullscreenExit />}
+                </div>
             <h3 className="font-bold">{props.name}</h3>
             <div className="w-64 h-64 flex items-center justify-center">
                 <img className="bg-red" src={props.imgUrl} alt={props.name} width={200}  />
